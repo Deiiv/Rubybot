24b0d91e1947e84b53058284a256fb30a859a55c
Deiv <deiv.apps@gmail.com>
2020-04-19 14:46:31 -0400

    adding reaction roles, removing command role set, removing admin commands

--- archive/handleActionSetRole.js
+++ archive/handleActionSetRole.js
@@ -1,7 +1,23 @@
+/*
+
+
+
+
+
+ARCHIVED
+
+Not in user
+
+
+
+
+
+*/
+
 const Discord = require("discord.js");
 const getValidRoles = require("./subActionFunctions/getValidRoles.js");
 
-var handleActionRemoveRole = function(msg) {
+var handleActionSetRole = function (msg) {
 	let guild = "Ruby";
 	try {
 		guild = msg.member.guild.name;
@@ -12,37 +28,42 @@ var handleActionRemoveRole = function(msg) {
 		return;
 	}
 	getValidRoles(guild)
-		.then(validRoles => {
-			let roleName = msg.content.substring(12);
+		.then((validRoles) => {
+			let roleName = msg.content.substring(9);
 
-			if (roleName && validRoles.find(r => r.toLowerCase() === roleName.toLowerCase())) {
-				let role = msg.guild.roles.find(role => role.name.toLowerCase() === roleName.toLowerCase());
-				if (msg.member.roles.find(r => r.name.toLowerCase() === roleName.toLowerCase())) {
+			if (roleName && validRoles.find((r) => r.toLowerCase() === roleName.toLowerCase())) {
+				if (msg.member.roles.find((r) => r.name.toLowerCase() === roleName.toLowerCase())) {
+					let message = new Discord.RichEmbed().setColor(process.env.embedColour).addField("You already have the " + roleName.toLowerCase() + " role", process.env.monkaThink);
+					msg.channel.send(message);
+					return;
+				}
+
+				let role = msg.guild.roles.find((role) => role.name.toLowerCase() === roleName.toLowerCase());
+				if (!role) {
+					let message = new Discord.RichEmbed().setColor(process.env.embedColour).addField("Role " + roleName.toLowerCase() + " not found", ":interrobang:");
+					msg.channel.send(message);
+				} else {
 					msg.member
-						.removeRole(role)
+						.addRole(role)
 						.then(() => {
-							let message = new Discord.RichEmbed().setColor(process.env.embedColour).addField("Done!", roleName.toLowerCase() + " role successfully removed " + process.env.peepoHappy);
+							let message = new Discord.RichEmbed().setColor(process.env.embedColour).addField("Done!", roleName.toLowerCase() + " role successfully set " + process.env.peepoHappy);
 							msg.channel.send(message);
 						})
-						.catch(error => {
+						.catch((error) => {
 							console.log(error);
 							let message = new Discord.RichEmbed().setColor(process.env.embedColour).addField("Encountered an error: " + error.message, ":interrobang:");
 							msg.channel.send(message);
 						});
-				} else {
-					let message = new Discord.RichEmbed().setColor(process.env.embedColour).addField("You don't have the " + roleName.toLowerCase() + " role", process.env.monkaThink);
-					msg.channel.send(message);
-					return;
 				}
 			} else {
 				let message = new Discord.RichEmbed().setColor(process.env.embedColour).addField("Invalid role!", "Please send one of the following: " + validRoles.toString());
 				msg.channel.send(message);
 			}
 		})
-		.catch(error => {
+		.catch((error) => {
 			console.log(error);
 			let message = new Discord.RichEmbed().setColor(process.env.embedColour).addField("Encountered an error: " + error.message, ":interrobang:");
 			msg.channel.send(message);
 		});
 };
-module.exports = handleActionRemoveRole;
+module.exports = handleActionSetRole;
---
+++ subfunctions/handleMessageReaction.js
@@ -0,0 +1,79 @@
+const Discord = require("discord.js");
+const handleProfEvent = require("./actionfunctions/subActionFunctions/handleProfEvent.js");
+
+var handleMessageReaction = function (reaction, user, type) {
+	// ignore invalid messages
+	if (!user) return;
+	if (user.bot) return;
+	if (!reaction.message.channel.guild) return;
+	if (reaction.message.channel.guild != "Deiv's Dev Corner" && reaction.message.channel.guild != "Ruby") return;
+	if (reaction.message.channel.name != "information");
+
+	let reactionName = reaction.emoji.name;
+	reactionName = reactionName.replace("_role", "");
+
+	if (type === "add") {
+		// asking to add but already have, ignore
+		if (reaction.message.guild.member(user).roles.find((r) => r.name.toLowerCase() === reactionName.toLowerCase())) {
+			console.log(`Role ${reactionName} already set`);
+			return;
+		}
+
+		let role = reaction.message.guild.roles.find((role) => role.name.toLowerCase() === reactionName.toLowerCase());
+		if (!role) {
+			console.log(`Invalid role reaction : ${reactionName}`);
+			return;
+		}
+		reaction.message.guild
+			.member(user)
+			.addRole(role)
+			.then(() => {
+				console.log(`Set role ${reactionName} to user ${reaction.message.member.displayName}`);
+				// if the role is "ruby" then udpate user in db with ruby as guild
+				if (reactionName.toLowerCase() === "ruby") {
+					let params = {
+						username: reaction.message.member.displayName,
+						userid: reaction.message.author.id,
+						action: "updateuser",
+						guild: "Ruby",
+					};
+					handleProfEvent(params)
+						.then(() => {
+							console.log("Done updating user in db");
+						})
+						.catch((error) => {
+							console.log(error);
+						});
+				}
+				// let message = new Discord.RichEmbed().setColor(process.env.embedColour).addField("Done!", reactionName.toLowerCase() + " role successfully set " + process.env.peepoHappy);
+				// reaction.message.channel.send(message);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	} else if (type === "remove") {
+		// asking to remove but it's already gone, ignore
+		if (!reaction.message.guild.member(user).roles.find((r) => r.name.toLowerCase() === reactionName.toLowerCase())) {
+			console.log(`Role ${reactionName} already removed, ignoring`);
+			return;
+		}
+
+		let role = reaction.message.guild.roles.find((role) => role.name.toLowerCase() === reactionName.toLowerCase());
+		if (!role) {
+			console.log(`Invalid role reaction : ${reactionName}`);
+			return;
+		}
+		reaction.message.guild
+			.member(user)
+			.removeRole(role)
+			.then(() => {
+				console.log(`Removed role ${reactionName} from user ${reaction.message.member.displayName}`);
+				// let message = new Discord.RichEmbed().setColor(process.env.embedColour).addField("Done!", reactionName.toLowerCase() + " role successfully set " + process.env.peepoHappy);
+				// reaction.message.channel.send(message);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}
+};
+module.exports = handleMessageReaction;
