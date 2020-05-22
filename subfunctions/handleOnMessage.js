e9b6d112ac8804e8e5bcf3b13c35670c6d3354c1
Deiv <deiv.apps@gmail.com>
2020-05-21 22:56:00 -0400

    update

--- subfunctions/handleOnMessage.js
+++ subfunctions/handleOnMessage.js
@@ -46,12 +46,12 @@ var handleOnMessage = function (msg) {
 			handleActionContact(msg);
 			break;
 		case "!test":
-			let message = "Head over to xyz to read our rules/info and set the Ruby role (required for viewing channels) " + process.env.pogpeach;
+			let message = ", please head over to xyz to read our rules/info and set the Ruby role (required for viewing channels) " + process.env.pogpeach;
 			// let welcomeMessageEmbed = new Discord.MessageEmbed().setColor(process.env.embedColour).addField("Welcome <@" + msg.author.id + ">, " + process.env.pepeRuby, "<@" + msg.author.id + ">, " + message);
 			let welcomeMessageEmbed = new Discord.MessageEmbed()
 				.setColor(process.env.embedColour)
 				.setTitle(`Welcome ${msg.member.displayName}! ` + process.env.pepeRuby)
-				.setDescription(message);
+				.setDescription(`${msg.author}` + message);
 			msg.channel.send(welcomeMessageEmbed);
 			break;
 		default:
