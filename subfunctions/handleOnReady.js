03b24bfe1839112752839b962507094424d78fe8
Deiv <deiv.apps@gmail.com>
2020-11-08 20:14:58 -0500

    adding new feature for dedicated bot channel

--- subfunctions/handleOnReady.js
+++ subfunctions/handleOnReady.js
@@ -17,10 +17,11 @@ var handleOnReady = function (client) {
 	process.env.pepoG = client.emojis.cache.find((emoji) => emoji.name === "pepoG") || "";
 	process.env.pogpeach = client.emojis.cache.find((emoji) => emoji.name === "pogpeach") || "";
 	process.env.ruby = client.emojis.cache.find((emoji) => emoji.name === "ruby") || "";
+	process.env.monkaO = client.emojis.cache.find((emoji) => emoji.name === "monkaO") || "";
 	// these are defined here so that .env can be hidden, but still have customizable values stored in git
 	process.env.adminUserTag = "<@140904638084808705>";
 	process.env.embedColour = "#FEC6C7";
-	process.env.botversion = 9.03;
+	process.env.botversion = 9.04;
 	process.env.runtime = "Node.Js 12.x | Discord.Js 12.x";
 	process.env.host = "AWS";
 	process.env.author = "Deiv";
