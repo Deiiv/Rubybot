6d0a6d10d56295b5f5db4c43b1e3606173530da8
David Fedorovsky <david.fedorovsky@gmail.com>
2025-04-30 20:20:55 -0400

    Adding conditional celface

--- subfunctions/handleOnMessage.js
+++ subfunctions/handleOnMessage.js
@@ -121,13 +121,11 @@ var handleOnMessage = function (msg) {
 			// if (process.env.phoebepog) msg.react(process.env.phoebepog);
 			if (process.env.phoepeek) msg.react(process.env.phoepeek);
 		}
-		/*
-		if (msg.guild.id === "375518646015098893" && msg.content.toLowerCase().indexOf("cel") != -1) {
+		if (msg.guild.id === "375518646015098893" && (msg.content.toLowerCase().statsWith("cel ") || msg.content.toLowerCase().endsWith(" cel") || msg.content.toLowerCase().includes(" cel ") || msg.content.toLowerCase().includes(" cel."))) {
 			// only add reaction if the emoji exists in bot memory
 			// if (process.env.phoebepog) msg.react(process.env.phoebepog);
 			if (process.env.celface) msg.react(process.env.celface);
 		}
-		*/
 	}
 };
 module.exports = handleOnMessage;
